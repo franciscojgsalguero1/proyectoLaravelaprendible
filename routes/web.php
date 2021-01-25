0d9f70b41cd711668e46fe1390b62c575a7f7c4e
Francisco José Gordo Salguero <fgordo@cifpfbmoll.eu>
2021-01-25 14:04:40 +0100

    Cambio de añadiendo rutas en routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+// aprendible.com = Route::get('/', function);
+// aprendible.com/contacto = Route::get('contacto', function);
+/*
+Route::get('/', function() {
+    return "Hola desde la página de inicio";
 });
+
+Route::get('contacto', function() {
+    return "Hola desde la página de contacto";
+});
+
+// en saludo1 es obligatorio pasarle un parámetro
+Route::get('saludo1/{nombre}', function($nombre) {
+   return "Saludos " . $nombre; 
+});
+
+// en saludo2 no es obligatorio pasarle un parámetro
+Route::get('saludo2/{nombre?}', function($nombre = "Invitado") {
+   return "Saludos " . $nombre; 
+});
+
+// Route::get(); podemos atender peticiones con post
+
+// Rutas con Nombre
+
+Route::get('contactame', function() {
+    return "Sección de contactos";
+})->name('contactos');
+
+Route::get('/', function() {
+    echo "<a href='". route('contactos') ."'>Contactos 1</a><br>";
+    echo "<a href='". route('contactos') ."'>Contactos 2</a><br>";
+    echo "<a href='". route('contactos') ."'>Contactos 3</a><br>";
+    echo "<a href='". route('contactos') ."'>Contactos 4</a><br>";
+    echo "<a href='". route('contactos') ."'>Contactos 5</a><br>";
+});
+
+
+// Devolvemos paginas web html
+
+Route::get('/', function() {
+    $nombre = "Jorge";
+    return view('home')->with('nombre', $nombre);
+    //return view('home')->with(['nombre'=> $nombre]);
+    //return view('home', ['nombre'=> $nombre]);
+    //return view('home', compact('nombre'));
+})->name('home');*/
+
+Route::view('/', 'home')->name('home');
+Route::view('/about','about')->name('about');
+Route::view('/portofolio','portofolio')->name('portofolio');
+Route::view('/contact','contact')->name('contact');
