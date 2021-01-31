3a427bc5bd6ece69a115076b210457ced1062570
Francisco José Gordo Salguero <fgordo@cifpfbmoll.eu>
2021-01-31 21:38:53 +0100

    Hecho el curso de aprendible hasta el 14 incluido

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,4 +4,17 @@
 
 @section('content')
     <h1>Contact</h1>
+    <form method="post" action="{{route('contact')}}">
+        @csrf
+        {{ $errors }}<br>
+        {{ var_dump($errors->any())}}<br>
+        @if($errors->any())
+        <p> Mostrar errores </p><br>
+        @endif
+        <input name="name" placeholder="Nombre... "><br>
+        <input type="email" name="email" placeholder="email"><br>
+        <input name="subject" placeholder="Asunto... "><br>
+        <textarea name="contect" placeholder="Mensaje... "></textarea><br>
+        <button>Enviar</button>
+    </form>
 @endsection
